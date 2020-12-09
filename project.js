1aa778064098c1f89c1599eceab8ada889dfa684
PanayotovaT <75318821+PanayotovaT@users.noreply.github.com>
2020-12-09 13:06:40 +0200

    Delete project.js

--- project.js
+++
@@ -1,7 +0,0 @@
-function project(name,numberOfProjects){
- let timeNeeded = numberOfProjects * 3;
- console.log(`The architect ${name} will need ${timeNeeded} hours to complete ${numberOfProjects} project/s.`);
-
-}
-
-project("Sanya", "9")
