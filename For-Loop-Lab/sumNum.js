0d17cb7ae8b127c6656e66ece9db358a8dc8fd5c
PanayotovaT <75318821+PanayotovaT@users.noreply.github.com>
2020-12-19 17:42:56 +0200

    Add files via upload

---
+++ For-Loop-Lab/sumNum.js
@@ -0,0 +1,15 @@
+function solve(n){
+
+    
+    let sum = 0;
+    for (let i = 0; i < n.length; i++) {
+
+    
+        sum += Number(n[i]);
+
+    }
+    console.log(`The sum of the digits is:${sum}`);
+
+}
+
+solve("564891");
